7956aafd7c8656a3a6e4130f4e7d02030937d01c
Alan Araujo <alan.araujo@unicid.edu.br>
2020-01-29 11:45:16 -0300

    Laço de repeticao utilizando for e forIn

--- lacosrepeticao.js
+++ lacosrepeticao.js
@@ -1,14 +1,32 @@
+var Fruits = /** @class */ (function () {
+    function Fruits() {
+    }
+    return Fruits;
+}());
 window.onload = function () {
-    var i = 10;
+    var i;
     var textOut = "";
+    var myArray = ["cat", "dog", 223, "fish", true, "bat"];
     // while (i < 10){
     //     textOut += "o numero é: " + i + "<br>";
     //     i++;
     // }
     // document.getElementById('content').innerHTML = textOut;
-    do {
-        textOut += "o numero é: " + i + "<br>";
-        i++;
-    } while (i < 10);
-    document.getElementById('content').innerHTML = textOut;
+    // do{
+    //     textOut += "o numero é: " + i + "<br>";
+    //     i++;
+    // } while (i < 10)
+    // document.getElementById('content').innerHTML = textOut;
+    // for (i = 0; i < 5; i++) {
+    //     textOut += "o numero é: " + i + "<br>";
+    // }
+    for (i = 0; i < 5; i++) {
+        textOut += myArray[i] + "<br>";
+    }
+    var maca = { name: "Apple", color: "Red", size: 5 };
+    var text = "";
+    for (var f in maca) {
+        text += maca[f];
+    }
+    document.getElementById('content').innerHTML = text;
 };
--- lacosrepeticao.ts
+++ lacosrepeticao.ts
@@ -1,6 +1,13 @@
+class Fruits {
+    name: string;
+    color: string;
+    size: number;
+}
+
 window.onload = () => {
-    var i: number = 10;
+    var i: number;
     var textOut: string = "";
+    var myArray: Array<any> = ["cat", "dog", 223, "fish", true, "bat"];
 
     // while (i < 10){
     //     textOut += "o numero é: " + i + "<br>";
@@ -9,12 +16,27 @@ window.onload = () => {
 
     // document.getElementById('content').innerHTML = textOut;
 
-    do{
-        textOut += "o numero é: " + i + "<br>";
-        i++;
-    } while (i < 10)
+    // do{
+    //     textOut += "o numero é: " + i + "<br>";
+    //     i++;
+    // } while (i < 10)
         
-    document.getElementById('content').innerHTML = textOut;
+    // document.getElementById('content').innerHTML = textOut;
     
+    // for (i = 0; i < 5; i++) {
+    //     textOut += "o numero é: " + i + "<br>";
+    // }
 
+    for (i = 0; i < 5; i++) {
+        textOut += myArray[i]+"<br>";
+    }
+
+    var maca: Fruits = {name: "Apple", color: "Red", size: 5};
+    var text: string = "";
+
+    for (const f in maca) {
+        text += maca[f];
+    }
+
+    document.getElementById('content').innerHTML = text;
 }
